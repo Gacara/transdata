d7275e2f745af8c15965450f794378e389b908dc
Romain Goulet <romain.goulet@edu.devinci.fr>
2020-10-20 22:03:42 +0200

    update yarn with typescript

--- src/callAPI/async.tsx.tsx
+++ src/callAPI/async.tsx.tsx
@@ -8,6 +8,16 @@ interface IState {
 interface IErrorProps {
   error: Error;
 }
+
+interface ErrorOverideInterface {
+  renderError?: React.Component<IErrorProps, {}, any> | undefined
+  error: Error;
+}
+
+interface LoadingOverrideInterface {
+  renderLoading?: React.Component<any>;
+}
+  
   
 interface IAsyncProps {
   state: IState;
@@ -24,14 +34,25 @@ const DefaultLoadingComponent = () => {
   return <p>Loading...</p>
 }
 
+function ErrorOverride({renderError}: ErrorOverideInterface) {
+  return <>{renderError ?
+    renderError
+  :
+  DefaultErrorComponent
+  }</>
+}
+
+function LoadingOverride({renderLoading}: LoadingOverrideInterface) {
+  return <>{renderLoading ? renderLoading : DefaultLoadingComponent}</>;
+}
+
 export default ({ state, renderError: ErrorComponent, renderLoading: LoadingComponent, children }: IAsyncProps) => {
-  const ErrorOverride = ErrorComponent || DefaultErrorComponent;
-  const LoadingOverride = LoadingComponent || DefaultLoadingComponent;
+
   
   if (state.error) {
-    return <ErrorOverride error={state.error} />
+    return <ErrorOverride error={state.error} renderError={ErrorComponent} />
   } else if (state.isLoading) {
-    return <LoadingOverride />
+    return <LoadingOverride renderLoading={LoadingComponent} />
   } else {
     return children
   }
